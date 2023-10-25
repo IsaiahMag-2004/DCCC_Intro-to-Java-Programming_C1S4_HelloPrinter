d86748ef1248b49ed5e73a8be9d61392dea469ad
Isaiah Magana <isaiahmag@Isaiahs-MacBook-Air.local>
2023-10-25 01:08:31 -0400

    Create the first program of the textbook with complete documentation

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * Create our first exercise of the textbook which prints out the greeting "Hello World"
+ * @author isaiahmag
+ *
+ * Main.java
+ */
+public class Main {
+    /**
+     * Main Method to run program
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
